ae5e11dfb2981cfd25f5af531893ce768223dcd5
ZaStack <stackzpersonal@gmail.com>
2020-05-23 12:35:19 -0700

    Builds plant and species table. Working on endpoints to test

--- data/migrations/20200523022542_plants.js
+++ data/migrations/20200523022542_plants.js
@@ -1,25 +1,43 @@
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('users', (users) => {
+            users.increments();
 
-exports.up = function(knex) {
-  return knex.schema
-  .createTable('users', users => {
-      users.increments();
-      
-      users.string('username', 255)
-      .notNullable()
-      .unique();
-      
-      users.string('email', 255)
-      .notNullable()
-      .unique();
-      
-      users.string('password', 255)
-      .notNullable();
+            users.string('username', 255).notNullable().unique();
 
-    });
+            users.string('email', 255).notNullable().unique();
+
+            users.string('password', 255).notNullable();
+        })
+        .createTable('plants', (plants) => {
+            plants.increments();
+            plants
+                .integer('user_id')
+                .notNullable()
+                .references('id')
+                .inTable('users')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE');
+            plants.string('nickname').notNullable().unique();
+            plants.string('image');
+        })
+        .createTable('species', (species) => {
+            species.increments();
+            species
+                .integer('plant_id')
+                .notNullable()
+                .references('id')
+                .inTable('plants')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE');
+            species.string('species').notNullable().unique();
+            species.integer('h2o_frequency').notNullable();
+        });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
     return knex.schema
-    .dropTableIfExists('users')
-  
+        .dropTableIfExists('species')
+        .dropTableIfExists('plants')
+        .dropTableIfExists('users');
 };
---
+++ plants/plants-router.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const Plants = require('./plants-model')
+
+const router = express.Router()
+
+router.get('/plants', (req, res) => {
+    Plants.findPlants().then(plants => {
+        res.json(plants)
+    }).catch(err => {
+        res.status(500).json({ message: 'Failed to get plants' })
+    })
+})
+
+router.get('/species', (req, res) => {
+    Plants.findSpecies().then(species => {
+        res.json(species)
+    }).catch(err => {
+        res.status(500).json({ message: 'Failed to get species' })
+    })
+})
+
+router.get('/plants/:id', (req, res) => {
+    const { id } = req.params
+
+    Plants.findPlantById(id).then(plant => {
+        if (plant) {
+            res.json(plant)
+        } else {
+            res.status(404).json({ message: 'Could not find plant with given id' })
+        }
+    }).catch(err => {
+        res.status(500).json({ message: 'Failed to get plant' })
+    })
+})
+
+router.get('/species/:id', (req, res) => {
+    const { id } = req.params
+
+    Plants.findSpeciesById(id).then(species => {
+        if (species) {
+            res.json(species)
+        } else {
+            res.status(404).json({ message: 'Could not find species with given id' })
+        }
+    }).catch(err => {
+        res.status(500).json({ message: 'Failed to get species' })
+    })
+})
+
