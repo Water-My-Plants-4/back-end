e72426496b7b6d02562da0795bf4a8e687bd946f
ZaStack <stackzpersonal@gmail.com>
2020-05-23 13:33:46 -0700

    Finishes adding plant and species endpoints

--- plants/plants-router.js
+++ plants/plants-router.js
@@ -70,3 +70,73 @@ router.post('/plants', (req, res) => {
             res.status(500).json({ message: 'Failed to add plant' });
         });
 });
+
+router.post('/species', (req, res) => {
+    const speciesData = req.body
+
+    Plants.addSpecies(speciesData).then((species) => {
+        res.status(201).json(species)
+    }).catch(err => {
+        res.status(500).json({ message: 'Failed to add species' })
+    })
+})
+
+router.put('/plants/:id', (req, res) => {
+    const { id } = req.params
+    const changes = req.body
+    
+    Plants.findPlantById(id).then(plant => {
+        if (plant) {
+            Plants.updatePlant(changes, id).then(updatedPlant => {
+                res.json(updatedPlant)
+            })
+        } else {
+            res.status(404).json({ message: 'Could not find plant with given id'})
+        }
+    }).catch(err => {
+        res.status(500).json({ message: 'Failed to update plant'})
+    })
+})
+
+router.put('/species/:id', (req, res) => {
+    const { id } = req.params
+    const changes = req.body
+    
+    Plants.findSpeciesById(id).then(species => {
+        if (species) {
+            Plants.updateSpecies(changes, id).then(updatedSpecies => {
+                res.json(updatedSpecies)
+            })
+        } else {
+            res.status(404).json({ message: 'Could not find species with given id'})
+        }
+    }).catch(err => {
+        res.status(500).json({ message: 'Failed to update species' })
+    })
+})
+
+router.delete('/plants/:id', (req, res) => {
+    const { id } = req.params
+
+    Plants.removePlant(id).then(deleted => {
+        if (deleted) {
+            res.json({ removed: deleted })
+        } else {
+            res.status(404).json({ message: 'Could not find plant with given id' })
+        }
+    }).catch(err => {
+        res.status(500).json({ message: 'Failed to delete plant' })
+    })
+})
+
+router.delete('/species/;id', (req, res) => {
+    Plants.removeSpecies(id).then(deleted => {
+        if (delted) {
+            res.json({ removed: deleted })
+        } else {
+            res.status(404).json({ message: 'Could not find species with given id' })
+        }
+    }).catch(err => {
+        res.status(500).json({ message: 'Failed to delete species' })
+    })
+})
